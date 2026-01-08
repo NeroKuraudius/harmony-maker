4d70b8ff68b63b25473be24bf8a47fb5600ec359
Nero Hsu <nero.hsu@inspiredgt.local>
2026-01-08 11:28:16 +0800

    feat: add router for monitor

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,10 @@ const line = require('./modules/line')
 
 router.use('/line', line)
 
+router.get('/healthz', (req,res)=>{
+  return res.status(200).send('ok')
+})
+
 router.get('/', (req,res)=>{
   return res.sendFile(path.join(__dirname, '..', '..', 'views', 'default.html'))
 })
